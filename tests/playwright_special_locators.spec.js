48ac89447f39e7a97ca8c9cc1e98c55c01d42660
Vishvambruth J T <vishruth.cse@gmail.com>
2026-09-20 00:12:47 +0530

    Updated Framework

--- tests/playwright_special_locators.spec.js
+++ tests/playwright_special_locators.spec.js
@@ -48,3 +48,33 @@ test('Playwright test level timeout', async ({page}) => {
     await slowExpect(page.locator(".my-4").first()).toHaveText('Shop Name');
     await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();   
 });
+
+test('Client App login', async ({ page }) => {   
+   const email = "anshika@gmail.com";
+   const productName = 'ZARA COAT 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.getByPlaceholder("email@example.com").fill(email);
+   await page.getByPlaceholder("enter your passsword").fill("Iamking@000");
+   await page.getByRole('button',{name:"Login"}).click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   
+   await page.locator(".card-body").filter({hasText:"ZARA COAT 3"})
+   .getByRole("button",{name:"Add to Cart"}).click();
+ 
+   await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+ 
+   //await page.pause();
+   await page.locator("div li").first().waitFor();
+   await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+ 
+   await page.getByRole("button",{name :"Checkout"}).click();
+ 
+   await page.getByPlaceholder("Select Country").pressSequentially("ind");
+ 
+   await page.getByRole("button",{name :"India"}).nth(1).click();
+   await page.getByText("PLACE ORDER").click();
+ 
+   await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+})
